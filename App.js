11c30b56c453fe494609b4a9cc88aeaffde4aa95
Akash Sharma <125081789+akash567-ak@users.noreply.github.com>
2025-07-21 08:18:29 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-    
-    
-    const parent = React.createElement("div", { id: "parent" }, [
-        React.createElement("div", { id: "child" }, [
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag"),
-        ]),
-        
-        React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am an h1 tag"), 
-        React.createElement("h2", {}, "I am an h2 tag"),
-        ])
-    ]);
-    
-    
-    
-    
-
-    console.log(parent);
-        
-    const root = ReactDOM.createRoot(document.getElementById("root"));
-        
-    root.render(parent);
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./src/components/Header"; 
